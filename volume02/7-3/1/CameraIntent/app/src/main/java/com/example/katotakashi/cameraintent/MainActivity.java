eb57c6f878cde463860a96900ee167bac2019636
Kato Takashi <taka4.kato@gmail.com>
2015-04-08 22:43:21 +0900

    P128 7-3-3

--- volume02/7-3/1/CameraIntent/app/src/main/java/com/example/katotakashi/cameraintent/MainActivity.java
+++ volume02/7-3/1/CameraIntent/app/src/main/java/com/example/katotakashi/cameraintent/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.katotakashi.cameraintent;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,57 +14,41 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    Button button1;
+    ImageView imageView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
 
+        button1 = (Button) findViewById(R.id.button1);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                カメラでの撮影画面を暗黙のIntentで呼び出し
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                結果を受け取るためにstartActivityForResult(intent, requestCode)を使う
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK){
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            imageView1.setImageBitmap(image);
         }
     }
+
+
 }
