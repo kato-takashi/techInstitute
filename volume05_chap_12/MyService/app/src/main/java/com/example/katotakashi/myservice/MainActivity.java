b6dc0f3803d7458454adc2e1cbc955d69f474d1a
TK_Ratina <taka4.kato@gmail.com>
2015-05-12 14:29:44 +0900

    P78 「Bind」「Unbind」に対応した「Service」を作ってみましょう。

--- volume05_chap_12/MyService/app/src/main/java/com/example/katotakashi/myservice/MainActivity.java
+++ volume05_chap_12/MyService/app/src/main/java/com/example/katotakashi/myservice/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.katotakashi.myservice;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -19,21 +24,33 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button btn = (Button) findViewById(R.id.startBtn);
-        btn.setOnClickListener(btnListener);;
+        btn.setOnClickListener(btnListener);
+        ;
 
-         btn = (Button) findViewById(R.id.stopBtn);
-        btn.setOnClickListener(btnListener);;
+        btn = (Button) findViewById(R.id.stopBtn);
+        btn.setOnClickListener(btnListener);
+        ;
     }
 
     private View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.startBtn:
                     startService(new Intent(MainActivity.this, MyService.class));
                     break;
                 case R.id.stopBtn:
                     stopService(new Intent(MainActivity.this, MyService.class));
+                    break;
+                case R.id.bindBtn://doBindService
+                    doBindService();
+                    break;
+
+                case R.id.unBindBtn://doUnbindService
+                    doUnbindService();
+                    break;
+
+                default:
                     break;
             }
 
@@ -58,7 +75,46 @@ public class MainActivity extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    //取得したServiceの保存
+    private MyService mBoundService;
+    private boolean mIsBound;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+
+            // サービスとの接続確立時に呼び出される
+            Toast.makeText(MainActivity.this, "Activity:onServiceConnected", Toast.LENGTH_SHORT).show();
+
+            // サービスにはIBinder経由で#getService()してダイレクトにアクセス可能
+            mBoundService = ((MyService.MyServiceLocalBinder) service).getService();
+            //必要であればmBoundServiceを使ってバインドしたサービスへの制御を行う
+        }
+
+
+        public void onServiceDisconnected(ComponentName className) {
+            // サービスとの切断(異常系処理)
+            // プロセスのクラッシュなど意図しないサービスの切断が発生した場合に呼ばれる。
+            mBoundService = null;
+            Toast.makeText(MainActivity.this, "Activity:onServiceDisconnected",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    void doBindService() {
+        //サービスとの接続を確立する。明示的にServiceを指定
+        //(特定のサービスを指定する必要がある。他のアプリケーションから知ることができない = ローカルサービス)
+        bindService(new Intent(MainActivity.this, MyService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // コネクションの解除
+            unbindService(mConnection);
+            mIsBound = false;
+        }
     }
 }
--- volume05_chap_12/MyService/app/src/main/java/com/example/katotakashi/myservice/MyService.java
+++ volume05_chap_12/MyService/app/src/main/java/com/example/katotakashi/myservice/MyService.java
@@ -2,6 +2,7 @@ package com.example.katotakashi.myservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
@@ -11,6 +12,18 @@ import android.widget.Toast;
  */
 public class MyService extends Service {
     static final String TAG = "LocalService";
+
+    //サービスに接続するためのBinder
+    public class MyServiceLocalBinder extends Binder {
+        //サービスの取得
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    //Binderの生成
+    private final IBinder mBinder = new MyServiceLocalBinder();
+
     @Override
     public void onCreate() {
         Log.i(TAG, "onCreate");
@@ -32,6 +45,23 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Toast.makeText(this, "MyService#onBind" + ": " + intent, Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onBind" + ": " + intent);
+        return mBinder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Toast.makeText(this, "MyService#onRebind" + ": " + intent, Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onRebind" + ": " + intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Toast.makeText(this, "MyService#onUnbind" + ": " + intent, Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onUnbind" + ": " + intent);
+
+        //onUnbindをreturn trueでoverrideすると次回バインド時にonRebildが呼ばれる
+        return true;
     }
 }
